9145d0b3deed64a842e58cc1100f95c472a81849
Mateus Elias de Andrade Pereira <meap@cin.ufpe.br>
2023-03-12 09:24:12 -0300

    feat(script.js): setting game board on page load

---
+++ script.js
@@ -0,0 +1,43 @@
+var board;
+var score = 0;
+var rows = 4;
+var columns = 4;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let tile = document.createElement("div");
+            tile.id = i.toString() + "-" + j.toString();
+
+            let num = board[i][j];
+            updateTile(tile, num);
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = "";
+    tile.classList.add("tile");
+
+    if (num > 0) {
+        tile.innerText = num;
+        if (num < 4096) {
+            tile.classList.add("x" + num.toString());
+        } else {
+            tile.classList.add("x8192");
+        }
+    }
+}
